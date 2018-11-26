e1bf1fbf875a9fffefa94bf2bc8b6c590ee1bf57
Anton Zh <fanat.iko88@gmail.com>
2018-11-26 12:26:16 +0300

    wrong path burger pics slider

--- js/slider.js
+++ js/slider.js
@@ -3,34 +3,34 @@ $().ready(function(){
   let currentSlide = 0;
 
   let arrayImgList = ["image/burgers-slider/black_br.png", 
-  "../image/burgers-slider/chiken_br.png", 
-  "./image/burgers-slider/nyc_diner-baco_cheeseburger.png", 
-  "../../image/burgers-slider/spicy_shrimp_br.png",
-  "/image/burgers-slider/sandwich_br.png"]
+  "image/burgers-slider/chiken_br.png", 
+  "image/burgers-slider/nyc_diner-baco_cheeseburger.png", 
+  "image/burgers-slider/spicy_shrimp_br.png",
+  "image/burgers-slider/sandwich_br.png"]
 
   let odjectlist = [
     {
-      src: '../image/burgers-slider/black_br.png',
+      src: 'image/burgers-slider/black_br.png',
       burgerName: 'Dark Beef Burger',
       title: 'Ваш старый добрый знакомый, с рубленным бифштексом с 100% говядины, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
     },
     {
-      src: '../image/burgers-slider/chiken_br.png',
+      src: 'image/burgers-slider/chiken_br.png',
       burgerName: 'Chiken Burger',
       title: 'Ваш старый куриный бургер, с рубленным бифштексом с 100% говядины, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
     },
     {
-      src: '../image/burgers-slider/nyc_diner-baco_cheeseburger.png',
+      src: 'image/burgers-slider/nyc_diner-baco_cheeseburger.png',
       burgerName: 'NYC Diner-baco Cheeseburger',
       title: 'Ваш старый знакомый бургер "Нью-Йорк", с рубленным бифштексом с 100% говядины, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
     },
     {
-      src: '../image/burgers-slider/sandwich_br.png',
+      src: 'image/burgers-slider/sandwich_br.png',
       burgerName: 'Sandwich',
       title: 'Любимый сэндвич, с рубленным бифштексом с 100% говядины, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
     },
     {
-      src: '../image/burgers-slider/spicy_shrimp_br.png',
+      src: 'image/burgers-slider/spicy_shrimp_br.png',
       burgerName: 'Spicy Shripm Burger',
       title: 'Самый пикантный бургер, с рубленным бифштексом с 100% говядины, тремя кусочками нежнейшего сыра эмменталь, двумя помидорами, луком, салатом и соусом гриль теперь с добавлением экзотического соуса чураско.',
     },
