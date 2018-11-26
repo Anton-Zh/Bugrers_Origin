6cc808af87f3c8ceb91afce2ba4c12e1e0708fdd
Anton Zh <fanat.iko88@gmail.com>
2018-11-26 11:10:20 +0300

    order cod

--- js/form1.js
+++ js/form1.js
@@ -1,23 +1,25 @@
 $().ready(function(){
 
-  $('#myForm').on('submit', function(e){
-    e.preventDefault();
-    if($("#phone").text() !== ''){
-      $.ajax('https://webdev-api.loftschool.com/sendmail', {
-        type: "POST",
-        data: $(this).serializ(),
-        success: function(data){
-          console.log('done');
-          $(".modalOrder").css('display', 'block');
-          $(".modal__text").text('Произошла ошибка');
-          console.log(data);          
-        }
-      })
+  $('#myForm').on('submit', function(){ console.log('test')})
+  
+  // {
+  //   e.preventDefault();
+  //   if($("#phone").text() !== ''){
+  //     $.ajax('https://webdev-api.loftschool.com/sendmail', {
+  //       type: "POST",
+  //       data: $(this).serializ(),
+  //       success: function(data){
+  //         console.log('done');
+  //         $(".modalOrder").css('display', 'block');
+  //         $(".modal__text").text('Произошла ошибка');
+  //         console.log(data);          
+  //       }
+  //     })
 
-    }else {
-      $(".modalOrder").css('display', 'block');
-      $(".modal__text").text('Укажите свои данные');
-    }
-  })
+  //   }else {
+  //     $(".modalOrder").css('display', 'block');
+  //     $(".modal__text").text('Укажите свои данные');
+  //   }
+  // })
 
 });
